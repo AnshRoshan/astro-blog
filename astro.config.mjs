4d7f21440cce9a1ca0e490b4ca129c28780a92ab
❖Mʀ᭄Anshᴿᵒˢʰᵃⁿ࿐ <ianshroshan@gmail.com>
2025-04-09 18:26:25 +0530

    fix and changed design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://localhost.com',
-  integrations: [react()]
+	markdown: {
+		syntaxHighlighting: {
+			theme: "material-theme-palenight",
+			lineNumbers: true,
+			highlightLines: true,
+		},
+	},
+	site: "https://localhost.com",
+	integrations: [react(), tailwind()],
 });
